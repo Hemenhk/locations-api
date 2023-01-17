3ad9637c78e5518daf6bac7e17787f4e2c2a9e9c
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-01-17 11:52:19 +0000

    Remove code to find error

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Post
-from ratings.models import Rating
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -8,27 +7,15 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    ratings_id = serializers.SerializerMethodField()
-    ratings_count = serializers.ReadOnlyField()
-    reviews_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
     
-    def get_rating_id(self, obj):
-        user = self.context['request'].user
-        if user.is_authenticated:
-            rating = Rating.objects.filter(
-                owner=user, post=obj
-            ).first()
-            return rating.id if rating else None
-        return None
-
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'created_at', 'updated_at',
             'content', 'image', 'is_owner', 'profile_id', 'profile_image',
-            'image_filter', 'ratings_id', 'ratings_count', 'reviews_count', 'title',
+            'image_filter', 'title',
         ]
