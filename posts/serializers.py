64c38ba14640439288a30e655583786c4f3bb696
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-01-17 12:16:14 +0000

    Add back code after solving bug

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Post
 from ratings.models import Rating
 
+
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
@@ -9,7 +10,7 @@ class PostSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     rating_id = serializers.SerializerMethodField()
     ratings_count = serializers.ReadOnlyField()
-
+    reviews_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -27,11 +28,21 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-    
+
+        def get_rating_id(self, obj):
+            user = self.context['request'].user
+            if user.is_authenticated:
+                rating = Rating.objects.filter(
+                    owner=user, post=obj
+                ).first()
+                return rating.id if rating else None
+            return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'created_at', 'updated_at',
             'content', 'image', 'is_owner', 'profile_id', 'profile_image',
-            'image_filter', 'title', 'rating_id', 'ratings_count',
+            'image_filter', 'title', 'rating_id', 'ratings_count', 
+            'reviews_count'
         ]
