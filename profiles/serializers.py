8fdb99efa3620a18d66c1b1a077b0b86e651111f
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-01-23 07:56:33 +0000

    Remove 'contact' and 'email' models

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -14,6 +14,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'email', 'name',
+            'id', 'owner', 'created_at', 'updated_at', 'name',
             'content', 'image', 'is_owner', 'posts_count'
         ]
